6ab2deb7bd3593bb11d48d01199538f2ec715fa6
Bronek Kozicki <brok@incorrekt.com>
2024-12-22 15:35:25 +0000

    Cleanup

--- cmake/TargetGenerator.cmake
+++ cmake/TargetGenerator.cmake
@@ -1,33 +1,32 @@
 # Target generator to enforce that a HEADER file compiles cleanly with no
 # additional includes
 
-set(EMPTY_TEST_TARGET_TEMPLATE "#include <>\nint main() {}\n")
-
 function(setup_target_for_header)
-  set(oneValueArgs SUFFIX HEADER SOURCE_ROOT)
-  set(multiValueArgs DEPENDENCIES)
-  cmake_parse_arguments(Generator "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    set(oneValueArgs SUFFIX HEADER SOURCE_ROOT)
+    set(multiValueArgs DEPENDENCIES)
+    cmake_parse_arguments(Generator "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-  if(NOT DEFINED Generator_HEADER)
-    message(FATAL_ERROR "HEADER must be set")
-  endif()
+    if(NOT DEFINED Generator_HEADER)
+        message(FATAL_ERROR "HEADER must be set")
+    endif()
 
-  if(NOT DEFINED Generator_SUFFIX)
-    # Guess from the HEADER suffix
-    string(REGEX REPLACE "^.*[\\.]([a-zA-Z]+)$" "\\\\.\\1" Generator_SUFFIX ${Generator_HEADER})
-  endif()
+    if(NOT DEFINED Generator_SUFFIX)
+        # Deduce from the HEADER suffix
+        string(REGEX REPLACE "^.*[\\.]([a-zA-Z]+)$" "\\\\.\\1" Generator_SUFFIX ${Generator_HEADER})
+    endif()
 
-  if(NOT DEFINED Generator_SOURCE_ROOT)
-    set(Generator_SOURCE_ROOT "${CMAKE_BINARY_DIR}/generated")
-  endif()
+    if(NOT DEFINED Generator_SOURCE_ROOT)
+        set(Generator_SOURCE_ROOT "${CMAKE_BINARY_DIR}/generated")
+    endif()
 
-  string(REPLACE "include <>" "include <${Generator_HEADER}>" file_content ${EMPTY_TEST_TARGET_TEMPLATE})
-  string(REGEX REPLACE "^(.*)${Generator_SUFFIX}$" "${Generator_SOURCE_ROOT}/\\1_Main.cpp" file_name ${Generator_HEADER})
-  string(REGEX REPLACE "[\\./]" "-" target_name ${Generator_HEADER})
+    set(FILE_TEMPLATE "#include <>\nint main() {}\n")
+    string(REPLACE "include <>" "include <${Generator_HEADER}>" file_content ${FILE_TEMPLATE})
+    string(REGEX REPLACE "^(.*)${Generator_SUFFIX}$" "${Generator_SOURCE_ROOT}/\\1_Main.cpp" file_name ${Generator_HEADER})
+    string(REGEX REPLACE "[\\./]" "-" target_name ${Generator_HEADER})
 
-  file(WRITE ${file_name} "${file_content}")
+    file(WRITE ${file_name} "${file_content}")
 
-  add_executable(${target_name} ${file_name})
+    add_executable(${target_name} ${file_name})
 
-  target_link_libraries(${target_name} ${Generator_DEPENDENCIES})
+    target_link_libraries(${target_name} ${Generator_DEPENDENCIES})
 endfunction()
